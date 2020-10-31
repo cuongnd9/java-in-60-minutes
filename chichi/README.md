0be90de2231cc8194d1e197040b6b818c3a81310
Duy-Cuong Tran <103cuong@gmail.com>
2020-10-31 15:14:08 +0700

    Spring Boot 💅 (30 minutes) (#3)

---
+++ chichi/README.md
@@ -0,0 +1,4 @@
+```sh
+mvn install
+./mvnw spring-boot:run
+```
---
+++ chichi/src/test/java/social/pet/chichi/ChichiApplicationTests.java
@@ -0,0 +1,13 @@
+package social.pet.chichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChichiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
